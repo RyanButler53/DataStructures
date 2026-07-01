4ea4d9d615f06f8460784fe5d76769cfee9c196f
RyanButler53 <rmbutler@outlook.com>
2026-07-01 13:27:46 -0700

    Add Interval Tree test with a custom interval type and add to readme

--- tests/intervalTree-test.cpp
+++ tests/intervalTree-test.cpp
@@ -1,7 +1,27 @@
 #include "spatial/intervalTree.hpp"
 #include <iostream>
+#include <algorithm>
+#include <ranges>
 #include <gtest/gtest.h>
 
+
+// Interval that holds some data
+class IntervalWithMidpoint : public ITree::Interval<float>{
+    // Data
+    float midpoint_;
+
+    public: 
+
+    IntervalWithMidpoint(float low, float high):
+        ITree::Interval<float>(low, high), 
+        midpoint_{static_cast<float>((high + low)/ 2.0)}{}
+    IntervalWithMidpoint(float low, float high, float data):
+        ITree::Interval<float>(low, high), midpoint_{data}{}
+    IntervalWithMidpoint(const IntervalWithMidpoint &other) = default;
+    ~IntervalWithMidpoint() = default;
+    float midpoint() const {return midpoint_;}
+    bool operator==(const IntervalWithMidpoint& other) const = default;
+};
 
 class IntervalTreeTest : public ::testing::Test {
 
@@ -44,4 +64,29 @@ TEST_F(IntervalTreeTest, SupersetsMax){
     std::vector<ITree::Interval<int>> supersets = itree_.findSupersets(-2147483648, 2147483647);
     ASSERT_EQ(supersets.size(), 1);
     EXPECT_EQ(supersets[0], ITree::Interval<int>(-2147483648, 2147483647));
+}
+
+class IntervalTreeWithData : public ::testing::Test {
+    protected:  
+    IntervalTree<IntervalWithMidpoint> itree_;
+
+    void SetUp() override {
+        std::array<float, 9> lower{1.1,  1.5, 1.8, 1.2, 2.1, 9.25, 17.5, 30.5, 18.1};
+        std::array<float, 9> upper{2.02, 2.5, 2.2, 2.2, 2.3, 15.1, 20.9, 38.2, 20.6};
+        std::array<float, 9> data {1.56, 2.0, 2.0, 1.6, 2.2,12.175,19.2,34.35,19.35};
+        for (size_t i = 0; i < 9; ++i) {
+            IntervalWithMidpoint bounds{lower[i], upper[i], data[i]};
+            itree_.insert(bounds);
+        }
+    }
+};
+
+TEST_F(IntervalTreeWithData, testData){
+    std::vector<IntervalWithMidpoint> intervals = itree_.findOverlaps(2.0);
+    ASSERT_EQ(intervals.size(), 4);
+    std::vector<float> midpoints;
+    std::transform(intervals.begin(), intervals.end(), std::back_inserter(midpoints), 
+    [](const IntervalWithMidpoint& i){return i.midpoint();});
+    std::ranges::sort(midpoints);
+    ASSERT_EQ(midpoints, std::vector<float>({1.56, 1.6, 2.0, 2.0}));
 }
